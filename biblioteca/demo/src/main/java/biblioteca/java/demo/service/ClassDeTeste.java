f5a44ca5b1648f33db7392fb542f6f57afc7efa7
heldlipe <helderfelipehffs08@gmail.com>
2024-11-25 20:52:09 -0300

    8.0
    alteração primária no service.

--- biblioteca/demo/src/main/java/biblioteca/java/demo/service/ClassDeTeste.java
+++ biblioteca/demo/src/main/java/biblioteca/java/demo/service/ClassDeTeste.java
@@ -1,6 +1,8 @@
-package biblioteca.java.demo.service;
+/* package biblioteca.java.demo.service;
 // tratamento para salvar 
 public class ClassDeTeste {
     
 }
+*/
+
 // cahamar repositorio private  e colocar os metodos (adicionar, excluir, realizar), CRUD.
